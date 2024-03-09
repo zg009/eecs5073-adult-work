f3604e1ac701d2d37de67c48671d909fe7890e24
Tristen Teague <tristendt5@gmail.com>
2024-03-08 18:19:25 -0600

    Many Changes
    Encoder:
    - Properly encodes with cat classes with OneHotEncoder
    - Turned numerical transformation into function
    
    - Combines both encoded data into a single dataframe returned in `combined_dataframe`
    
    -Added calculate performance function that gets requires statistics for step 1a and 1b
    
    -Cleaned up some code

--- main.py
+++ main.py
@@ -1,56 +1,100 @@
 from ucimlrepo import fetch_ucirepo 
 import pandas as pd  
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.tree import DecisionTreeClassifier, export_text
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, f1_score
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
 
-def sklearn_encoder(features: pd.DataFrame):
-    # select the category classes from features
-    cat_classes = features.select_dtypes(include=[object])
+# Encodes catergory classes with One Hot Encoder
+def category_encode(features: pd.DataFrame):
 
-    # do i need a labelencoder or can i just raw it?
-    # le = LabelEncoder()
-    # features_2 = features.apply(le.fit_transform)
-    # print(features_2.head())
-    enc = OneHotEncoder()
-    enc.fit(cat_classes)
-    # print(enc.categories_)
-    category_names = [category for arr in enc.categories_ for category in arr]
-    print(category_names)
-    onehotlabels = enc.transform(cat_classes).toarray()
-    new_df = pd.DataFrame(onehotlabels, )
-    # print(new_df.head)
-    # new_target = pd.DataFrame(onehotlabels)
-    # print(new_target.columns)
+    # Select the category classes from features
+    cat_classes = features.select_dtypes(include=[object])
     
+    # Setup Hot Encoder
+    enc = OneHotEncoder(sparse_output=False).set_output(transform="pandas")
+    enc.fit(cat_classes)
+    
+    encoded_cat_data = enc.fit_transform(cat_classes)
+    new_df = pd.DataFrame(encoded_cat_data, )
+
+    return new_df
+
+# Encode numerical classes using mean value to binary attribute
+def numeric_binary_transform(numeric_columns: pd.DataFrame):
+    encoded_numeric_col = numeric_columns
+    for col in numeric_columns:
+        mean = numeric_columns[col].mean()
+        encoded_numeric_col[col] = np.where(numeric_columns[col] > mean, 0, 1)
+
+    return encoded_numeric_col
+
+
 # factor this out
 def preprocessing(df: pd.DataFrame):
+
     df = df.dropna() # remove all rows with garbage
     assert(len(df.columns[df.isnull().any()].tolist()) == 0) # assert no more na values
-    target = df['income'] # Y value
-    # whoever included target column values with a period in them
-    # needs to be in prison
-    target = target.replace(r'\.$', value="", regex=True)
-    features = df[df.columns.difference(['income'])] # X values
-    # probably have to fix this encoding system
-    dummified = pd.get_dummies(features)
-    # print(dummified.columns)
-    return (dummified, target)
 
+    # Removes periods in income    
+    target = df['income'] # Y value
+    target = target.replace(r'\.$', value="", regex=True)
+
+    # X values. Removes income from data
+    features = df[df.columns.difference(['income'])] 
+
+    # Get numeric columns
+    numeric_columns = df.select_dtypes(include=['int64'])
+    # Transform to binary attribute
+    encoded_numeric_df = numeric_binary_transform(numeric_columns)
+
+    # Encoding category classes
+    encoded_cat_df = category_encode(features)
+
+    # Combine encoded numeric classes and encoded catergory classes
+    combined_dataframe = pd.concat([encoded_numeric_df, encoded_cat_df], axis=1)
+
+    return (combined_dataframe, target)
+
+def calc_performance(y_test, y_hat):
+
+    # Get accuracy score
+    accuracy = accuracy_score(y_test, y_hat)
+
+    # Generate confusion matrix to get TP and FP
+    tree_cm = confusion_matrix(y_test, y_hat)
+    TP = tree_cm[1][1]
+    FN = tree_cm[1][0]
+    FP = tree_cm[0][1]
+
+    # Calculate precision
+    precision = float(TP / (TP + FP))
+    # Calculate recall
+    recall = float(TP / (TP + FN))
+            
+    # Calculate F1
+    f1 = float((2 * precision * recall) / (precision + recall))
+
+    print(f'Accuracy: {accuracy}\n TP: {TP}\n FP: {FP}\n Precision: {precision}\n Recall: {recall}\n F1: {f1}\n')
+
+    return accuracy, TP, FP, precision, recall, f1
+
+# Tree classifier. Returns accuracy, TP rate, FP rate, precision, recall, and F1
 def decision_tree(X_train, X_test, y_train, y_test, dummified):
-    # decision tree
     decision_tree = DecisionTreeClassifier(random_state=42)
     decision_tree.fit(X_train, y_train)
     # this stinks, probably needs tweaking
     tree_rules = export_text(decision_tree, feature_names=list(dummified.columns))
     # print(tree_rules)
     # predicted y
-    y_hat = decision_tree.predict(X_test)
-    accuracy = accuracy_score(y_test, y_hat)
-    return accuracy
+    y_hat = decision_tree.predict(X_test)   
+
+    # Get performance statistics
+    statistics = calc_performance(y_test, y_hat)
+
+    return statistics
 
 def naive_bayes(X_train, X_test, y_train, y_test):
     # gnb model
@@ -58,9 +102,11 @@ def naive_bayes(X_train, X_test, y_train, y_test):
     gnb = GaussianNB()
     gnb.fit(X_train, y_train)
     gnb_hat = gnb.predict(X_test)
-    accuracy = accuracy_score(y_test, gnb_hat)
-    # print(accuracy)
-    return accuracy
+
+    # Get performance statistics
+    statistics = calc_performance(y_test, gnb_hat)
+
+    return statistics
 
 from sklearn.cluster import KMeans
 def k_means(clusters, X_train, algorithm="lloyd") -> KMeans:
@@ -90,51 +136,43 @@ def svm_classifier(X_train, X_test, y_train, y_test):
     y_hat = svm.predict(X_test)
     accuracy = accuracy_score(y_test, y_hat)
     # print(accuracy)
-    return accuracy
-    
+    return accuracy    
 
 # fetch dataset 
 adult = fetch_ucirepo(id=2) 
 
 df = pd.DataFrame(adult.data.original)
 
-# dummified, target = preprocessing(df)
-
-# tts for later
-# X_train, X_test, y_train, y_test = train_test_split(dummified, target, test_size=0.33, random_state=42)
-
-# dt_acc = decision_tree(X_train, X_test, y_train, y_test, dummified)
-# nb_acc = naive_bayes(X_train, X_test, y_train, y_test)
-
-# time for step 2
 # remove all the '?'
-# think this removes all the '?'
 df = df[~df.isin(['?']).any(axis=1)]
-# have to call his beforehand in case pandas decides to do more stupid magic
-# i love this terrible language and librarieS!!!
-df = df.dropna() # remove all rows with garbage
+df = df.dropna() # remove all rows with missing values
 assert(len(df.columns[df.isnull().any()].tolist()) == 0) # assert no more na values
-# find the numeric columns
+
+# find the numeric columns: age, fnlwgt, education-num, capital-gain, capital-loss, hours-per-week
 numeric_columns = df.select_dtypes(include=['int64'])
 
-# cycle through them and get teh mean then split into 0 or 1 based on greater or less
-# then reassign to original df
-for col in numeric_columns:
-    # garbage python library means i have to import numpy to do this since it doesnt natively work
-    # with series objects! :D
-    mean = numeric_columns[col].mean()
-    numeric_columns[col] = np.where(numeric_columns[col] > mean, 0, 1)
-    df[col] = numeric_columns[col]
-
-# actually split it
+# actually split it. Remove target column (income) from data.
 features, target = preprocessing(df)
+# print("df:\n", df)
+# print("Features:\n", features.dtypes)
+# print("Target:\n", target)
+
+#Split training and test data
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.33, random_state=42)
 
-    
-from sklearn.neural_network import MLPClassifier
-# iterations dont matter here, leaving it at 400 and fuck off
-mlp = MLPClassifier(random_state=42, max_iter=400)
-mlp.fit(X_train, y_train)
-y_hat = mlp.predict(X_test)
-accuracy = accuracy_score(y_test, y_hat)
-print(accuracy)
+# Decision Tree
+print("Performing decision tree classifer:")
+statistics = decision_tree(X_train, X_test, y_train, y_test, features)
+
+# Naive Bayes
+print("Performing naive bayes classifer:")
+statistics = naive_bayes(X_train, X_test, y_train, y_test)
+
+# CNN
+# from sklearn.neural_network import MLPClassifier
+# # iterations dont matter here, leaving it at 400 and fuck off
+# mlp = MLPClassifier(random_state=42, max_iter=400)
+# mlp.fit(X_train, y_train)
+# y_hat = mlp.predict(X_test)
+# accuracy = accuracy_score(y_test, y_hat)
+# print(accuracy)
